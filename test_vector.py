3b41f42228dae7b9edaf64c9799504e2bfc95da7
Ryan Wooster <rwooster@umich.edu>
2018-12-01 20:16:47 -0500

    Started swapping to Decimal for actual testing

--- test_vector.py
+++ test_vector.py
@@ -4,6 +4,10 @@ from vector import Vector
 
 class TestVector(unittest.TestCase):
 
+    def __compare(self, expected, results):
+        for e, r in zip(expected, results):
+            self.assertEqual(e, r)
+
     def test_addition(self):
         expected = Vector(4, 5, 6)
         result = Vector(1, 1, 1) + Vector(3, 4, 5)
@@ -32,38 +36,43 @@ class TestVector(unittest.TestCase):
         print(result)
 
     def test_normalization(self):
-        print("Normalizations:")
-        result = Vector(5.581, -2.136).normalize()
-        print(result)
+        expected = [Vector(0.9339352142,-0.3574423253)]
+        result = [Vector(5.581, -2.136).normalize()]
 
-        result = Vector(1.996, 3.108, -4.554).normalize()
-        print(result)
+        expected.append(Vector(0.3404012959,0.5300437012,-0.7766470448))
+        result.append(Vector(1.996, 3.108, -4.554).normalize())
+
+        self.__compare(expected, result)
 
     def test_dot(self):
-        expected = 9
-        result = Vector(1, 1, 1).dot(Vector(2, 3, 4))
-        self.assertEqual(expected, result)
+        expected = [9]
+        result = [Vector(1, 1, 1).dot(Vector(2, 3, 4))]
 
-        print("Inner products:")
-        print(Vector(7.887, 4.138).dot(Vector(-8.802, 6.776)))
-        print(Vector(-5.955, -4.904, -1.874).dot(Vector(-4.496, -8.755, 7.103)))
+        expected.append(-41.38228600)
+        result.append(Vector(7.887, 4.138).dot(Vector(-8.802, 6.776)))
+
+        expected.append(56.39717800)
+        result.append(Vector(-5.955, -4.904, -1.874).dot(Vector(-4.496, -8.755, 7.103)))
+
+        self.__compare(expected, result)
     
     def test_angle(self):
-        expected = 0
-        result = Vector(1, 1, 1).theta(Vector(1, 1, 1))
-        self.assertEqual(expected, result)
+        expected = [0]
+        result = [Vector(1, 1, 1).theta(Vector(1, 1, 1))]
 
-        expected = 90
-        result = Vector(0, 5).theta_degrees(Vector(5, 0))
-        self.assertEqual(expected, result)
+        expected.append(90)
+        result.append(Vector(0, 5).theta_degrees(Vector(5, 0)))
         
-        expected = 180
-        result = Vector(0, 5).theta_degrees(Vector(0, -5))
-        self.assertEqual(expected, result)
+        expected.append(180)
+        result.append(Vector(0, 5).theta_degrees(Vector(0, -5)))
 
-        print("Angles:")
-        print(Vector(3.183, -7.627).theta(Vector(-2.668, 5.319)))
-        print(Vector(7.35, 0.221, 5.188).theta_degrees(Vector(2.751, 8.259, 3.985)))
+        expected.append(3.0720263111831025)
+        result.append(Vector(3.183, -7.627).theta(Vector(-2.668, 5.319)))
+
+        expected.append(60.27581120560713)
+        result.append(Vector(7.35, 0.221, 5.188).theta_degrees(Vector(2.751, 8.259, 3.985)))
+
+        self.__compare(expected, result)
 
 
 if __name__ == "__main__":
