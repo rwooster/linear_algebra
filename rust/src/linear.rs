9abc9ae5c261db7e4438a837449ca9d38ee0f6d5
Ryan Wooster <ryan@rwooster.io>
2019-01-31 21:15:58 -0500

    Vector addition

--- rust/src/linear.rs
+++ rust/src/linear.rs
@@ -1,5 +1,7 @@
 use std::fmt;
+use std::ops::Add;
 
+#[derive(Debug)]
 pub struct LinearVector {
 	dimension: usize,
 	coordinates: Vec<i32>
@@ -11,8 +13,51 @@ impl fmt::Display for LinearVector {
 	}
 }
 
+impl PartialEq for LinearVector {
+    fn eq(&self, other: &Self) -> bool {
+        self.coordinates == other.coordinates
+    }
+}
+
+impl Add for LinearVector {
+    type Output = LinearVector;
+
+    fn add(self, other: Self) -> LinearVector {
+        LinearVector::new(self.coordinates.iter().zip(&other.coordinates).map(|(a, b)| a+b).collect())
+    }
+}
+
 impl LinearVector {
 	pub fn new(coordinates_in:Vec<i32>) -> LinearVector {
 		LinearVector {dimension:coordinates_in.len(), coordinates: coordinates_in}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_equality() {
+        let vector1 = LinearVector::new(vec!(1, 2, 3));
+        let vector2 = LinearVector::new(vec!(1, 2, 3));
+        assert!(vector1 == vector2);
+
+        let vector3 = LinearVector::new(vec!(1, 2, 4));
+        assert!(vector1 != vector3);
+
+        let vector4 = LinearVector::new(vec!(1, 2, 3, 5));
+        assert!(vector1 != vector4);
+    }
+
+    #[test]
+    fn test_addition() {
+        let vector1 = LinearVector::new(vec!(1, 2, 3));
+        let vector2 = LinearVector::new(vec!(1, 2, 3));
+        let expected_sum = LinearVector::new(vec!(2, 4, 6));
+
+        assert!(vector1 + vector2 == expected_sum);
+    }
+}
+
+
